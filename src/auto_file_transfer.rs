a2472c8853695dece33bc2368d70f23369283016
Mike Krueger <mkrueger@posteo.de>
2022-11-14 19:27:39 +0100

    Fixed bug in pattern recognizer.

--- src/auto_file_transfer.rs
+++ src/auto_file_transfer.rs
@@ -25,11 +25,16 @@ impl PatternRecognizer {
 
     pub fn push_ch(&mut self, ch: u8) -> bool {
         let p = self.pattern[self.cur_idx];
-        if p == ch || self.ignore_case && ch >= b'a' && ch <= b'z' && ch + (b'a' - b'A') == p {
+        if p == ch || self.ignore_case && ch >= b'a' && ch <= b'z' && ch - b'a' + b'A' == p {
             self.cur_idx += 1;
             if self.cur_idx >= self.pattern.len() {
                 self.cur_idx = 0;
                 return true;
+            }
+        } else {
+            if self.cur_idx > 0 {
+                self.cur_idx = 0;
+                return self.push_ch(ch);
             }
         }
         false
@@ -52,8 +57,8 @@ pub struct AutoFileTransfer {
 impl AutoFileTransfer {
     pub fn new() -> Self {
         Self {
-            zmodem_dl: PatternRecognizer::from(b"**\x18B00000000000000", true),
-            zmodem_ul: PatternRecognizer::from(b"**\x18B0100000023be50", true),
+            zmodem_dl: PatternRecognizer::from(b"\x18B00000000000000", true),
+            zmodem_ul: PatternRecognizer::from(b"\x18B0100000023be50", true),
         }
     }
 
@@ -70,5 +75,73 @@ impl AutoFileTransfer {
             return Some((ProtocolType::ZModem, false));
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod Tests {
+    use crate::auto_file_transfer::PatternRecognizer;
+
+    #[test]
+    fn test_pattern_recognizer() {
+        let mut test = PatternRecognizer::from(b"Name", false);
+
+        let mut result = false;
+        for b in b"Name" {
+            result = test.push_ch(*b)
+        };
+        assert_eq!(true, result);
+
+        let mut result = false;
+        for b in b"name" {
+            result = test.push_ch(*b)
+        };
+        assert_eq!(false, result);
+    }
+
+    #[test]
+    fn test_pattern_recognizer_ignore_case() {
+        let mut test = PatternRecognizer::from(b"Name", true);
+
+        let mut result = false;
+        for b in b"name" {
+            result = test.push_ch(*b)
+        };
+        assert_eq!(true, result);
+
+        let mut result = false;
+        for b in b"NaMe" {
+            result = test.push_ch(*b)
+        };
+        assert_eq!(true, result);
+
+        let mut result = false;
+        for b in b"Nmae" {
+            result = test.push_ch(*b)
+        };
+        assert_eq!(false, result);
+    }
+
+    #[test]
+    fn test_pattern_recognizer_recovery() {
+        let mut test = PatternRecognizer::from(b"name", false);
+
+        let mut result = false;
+        for b in b"namname" {
+            result = test.push_ch(*b)
+        };
+        assert_eq!(true, result);
+
+    }
+
+    #[test]
+    fn test_pattern_recognizer_invalid() {
+        let mut test = PatternRecognizer::from(b"name", false);
+
+        let mut result = false;
+        for b in b"n_a_m_e" {
+            result = test.push_ch(*b)
+        };
+        assert_eq!(false, result);
     }
 }
