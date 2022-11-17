452de805d0253dc6687bb7ba5c84add8ccc5f0f3
Mike Krueger <mkrueger@posteo.de>
2022-11-17 21:24:25 +0100

    Improved protocol selector UI.

--- src/ui/protocol_selector.rs
+++ src/ui/protocol_selector.rs
@@ -1,79 +1,42 @@
 use super::Message;
 use crate::protocol::ProtocolType;
-use iced::widget::{button, column, horizontal_space, row, text};
+use iced::widget::{button, column, horizontal_space, row, text, Row, Column};
 use iced::{Element, Length};
 
-pub fn view_protocol_selector<'a>(download: bool) -> Element<'a, Message> {
-    let button_width = 120;
-    let space = 8;
-    let left = 20;
+const button_width: u16 = 120;
+const space: u16 = 8;
+const left: u16 = 20;
 
-    column![
-        row![button("Cancel").on_press(Message::Back),]
-            .padding(4)
-            .spacing(8),
-        text(format!(
-            "Select {} protocol",
-            if download { "download" } else { "upload" }
-        ))
-        .size(40),
-        row![
-            horizontal_space(Length::Units(left)),
-            button("Zmodem")
-                .on_press(Message::SelectProtocol(ProtocolType::ZModem, download))
-                .width(Length::Units(button_width)),
-            horizontal_space(Length::Units(space)),
-            text("The standard protocol")
-        ],
-        row![
-            horizontal_space(Length::Units(left)),
-            button("ZedZap")
-                .on_press(Message::SelectProtocol(ProtocolType::ZedZap, download))
-                .width(Length::Units(button_width)),
-            horizontal_space(Length::Units(space)),
-            text("8k Zmodem")
-        ],
-        row![
-            horizontal_space(Length::Units(left)),
-            button("Xmodem")
-                .on_press(Message::SelectProtocol(ProtocolType::XModem, download))
-                .width(Length::Units(button_width)),
-            horizontal_space(Length::Units(space)),
-            text("Outdated protocol")
-        ],
-        row![
-            horizontal_space(Length::Units(left)),
-            button("Xmodem 1k")
-                .on_press(Message::SelectProtocol(ProtocolType::XModem1k, download))
-                .width(Length::Units(button_width)),
-            horizontal_space(Length::Units(space)),
-            text("Rarely used anymore")
-        ],
-        row![
-            horizontal_space(Length::Units(left)),
-            button("Xmodem 1k-G")
-                .on_press(Message::SelectProtocol(ProtocolType::XModem1kG, download))
-                .width(Length::Units(button_width)),
-            horizontal_space(Length::Units(space)),
-            text("Does that even exist?")
-        ],
-        row![
-            horizontal_space(Length::Units(left)),
-            button("Ymodem")
-                .on_press(Message::SelectProtocol(ProtocolType::YModem, download))
-                .width(Length::Units(button_width)),
-            horizontal_space(Length::Units(space)),
-            text("Ok but Zmodem is better")
-        ],
-        row![
-            horizontal_space(Length::Units(left)),
-            button("Ymodem-G")
-                .on_press(Message::SelectProtocol(ProtocolType::YModemG, download))
-                .width(Length::Units(button_width)),
-            horizontal_space(Length::Units(space)),
-            text("A fast Ymodem variant")
-        ]
-    ]
-    .spacing(8)
+
+fn create_button_row<'a>(msg: Message, title: &'static str, descr: &'static str)  -> Element<'a, Message>  {
+    Row::new()
+        .push(horizontal_space(Length::Units(left)))
+        .push(button(title)
+            .on_press(msg)
+            .width(Length::Units(button_width)))
+        .push(horizontal_space(Length::Units(space)))
+        .push(text(descr))
+        .into()
+}
+
+pub fn view_protocol_selector<'a>(download: bool) -> Element<'a, Message> {
+    
+    let header = Row::new()
+    .push(button("Cancel").on_press(Message::Back))
+    .padding(4)
+            .spacing(8);
+    Column::new() 
+    .push(header)
+    .push(text(format!("Select {} protocol", if download { "download" } else { "upload" } )).size(40))
+    .push(
+        Column::new()
+        .push(create_button_row(Message::SelectProtocol(ProtocolType::ZModem, download), "Zmodem", "The standard protocol"))
+        .push(create_button_row(Message::SelectProtocol(ProtocolType::ZedZap, download), "ZedZap", "8k Zmodem"))
+        .push(create_button_row(Message::SelectProtocol(ProtocolType::XModem, download), "Xmodem", "Outdated protocol"))
+        .push(create_button_row(Message::SelectProtocol(ProtocolType::XModem1k, download), "Xmodem 1k", "Rarely used anymore"))
+        .push(create_button_row(Message::SelectProtocol(ProtocolType::XModem1kG, download), "Xmodem 1k-G", "Does that even exist?"))
+        .push(create_button_row(Message::SelectProtocol(ProtocolType::YModem, download), "Ymodem", "Ok but Zmodem is better"))
+        .push(create_button_row(Message::SelectProtocol(ProtocolType::YModemG, download), "Ymodem-G", "A fast Ymodem variant"))
+        .spacing(8))
     .into()
 }
