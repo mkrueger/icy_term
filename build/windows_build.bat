c689ee14a346df8efdd19310daf6e169ecd25677
Mike Krueger <mkrueger@posteo.de>
2022-12-02 21:23:43 +0100

    Added file_id.diz

--- build/windows_build.bat
+++ build/windows_build.bat
@@ -4,4 +4,4 @@ cd ..\icy_term
 git pull
 cargo build --release
 del build\icy_term_windows_.zip
-powershell Compress-Archive target\release\icy_term.exe build\icy_term_windows_.zip
+powershell Compress-Archive "target\release\icy_term.exe,build\file_id.diz" build\icy_term_windows_.zip
