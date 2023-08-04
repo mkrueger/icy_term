3d531ab8d4c8472406cba2a15ae7dd8bec0434a8
Mike Krüger <mkrueger@posteo.de>
2023-08-04 19:20:43 +0200

    Fixed windows terminal in release mode.

--- src/ui/main_window_mod.rs
+++ src/ui/main_window_mod.rs
@@ -1,4 +1,3 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(unsafe_code, clippy::wildcard_imports)]
 
 use directories::ProjectDirs;
