55646eca1371ada58d15261a1982bce68eb0e202
Mike Krüger <mkrueger@posteo.de>
2023-08-06 10:48:48 +0200

    Added windows icon to exe.

---
+++ build.rs
@@ -0,0 +1,14 @@
+use {
+    std::{env, io},
+    winres::WindowsResource,
+};
+
+fn main() -> io::Result<()> {
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            // This path can be absolute, or relative to your crate root.
+            .set_icon("build/windows/app.ico")
+            .compile()?;
+    }
+    Ok(())
+}
