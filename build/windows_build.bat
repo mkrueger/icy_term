e28ee9e5241bde8eac725e1179cf27f2958fbf6d
Mike Krüger <mkrueger@posteo.de>
2023-08-06 19:53:56 +0200

    Fixed file_id.diz for windows build.

--- build/windows_build.bat
+++
@@ -1,7 +0,0 @@
-cd ..\..\icy_engine 
-git pull
-cd ..\icy_term
-git pull
-cargo build --release
-del build\icy_term_windows_.zip
-powershell Compress-Archive "target\release\icy_term.exe,build\file_id.diz" build\icy_term_windows_.zip
