6c92ef654403d13263a82e18a4632e1591633762
Mike Krüger <mkrueger@posteo.de>
2023-08-21 21:15:29 +0200

    Use modal dialogs for up/downloads.

--- src/ui/dialogs/protocol_selector.rs
+++ src/ui/dialogs/protocol_selector.rs
@@ -1,29 +1,50 @@
 use eframe::egui::{self, RichText};
+use egui_modal::Modal;
 use i18n_embed_fl::fl;
 
 use crate::protocol::TransferType;
 
 use crate::ui::{MainWindow, MainWindowMode};
 
-fn create_button_row(
-    ui: &mut egui::Ui,
-    window: &mut MainWindow,
-    protocol: TransferType,
-    download: bool,
-    title: &'static str,
-    descr: &str,
-) {
-    ui.with_layout(ui.layout().with_cross_justify(true), |ui| {
-        if ui
-            .selectable_label(
-                false,
-                RichText::new(format!("{title:15}{descr}")).family(egui::FontFamily::Monospace),
-            )
-            .clicked()
-        {
-            window.initiate_file_transfer(protocol, download);
-        }
-    });
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref PROTOCOL_TABLE: [(TransferType, String, String); 7] = [
+        (
+            TransferType::ZModem,
+            "Zmodem".to_string(),
+            fl!(crate::LANGUAGE_LOADER, "protocol-zmodem-description")
+        ),
+        (
+            TransferType::ZedZap,
+            "ZedZap".to_string(),
+            fl!(crate::LANGUAGE_LOADER, "protocol-zmodem8k-description"),
+        ),
+        (
+            TransferType::XModem,
+            "Xmodem".to_string(),
+            fl!(crate::LANGUAGE_LOADER, "protocol-xmodem-description")
+        ),
+        (
+            TransferType::XModem1k,
+            "Xmodem 1k".to_string(),
+            fl!(crate::LANGUAGE_LOADER, "protocol-xmodem1k-description")
+        ),
+        (
+            TransferType::XModem1kG,
+            "Xmodem 1k-G".to_string(),
+            fl!(crate::LANGUAGE_LOADER, "protocol-xmodem1kG-description")
+        ),
+        (
+            TransferType::YModem,
+            "Ymodem".to_string(),
+            fl!(crate::LANGUAGE_LOADER, "protocol-ymodem-description")
+        ),
+        (
+            TransferType::YModemG,
+            "Ymodem-G".to_string(),
+            fl!(crate::LANGUAGE_LOADER, "protocol-ymodemg-description")
+        )
+    ];
 }
 
 pub fn view_selector(
@@ -32,11 +53,8 @@ pub fn view_selector(
     _frame: &mut eframe::Frame,
     download: bool,
 ) {
-    let mut open = true;
-    let mut close = false;
-
     if ctx.input(|i| i.key_down(egui::Key::Escape)) {
-        open = false;
+        window.mode = MainWindowMode::ShowTerminal;
     }
 
     let title = RichText::new(if download {
@@ -44,84 +62,41 @@ pub fn view_selector(
     } else {
         fl!(crate::LANGUAGE_LOADER, "protocol-select-upload")
     });
+    let modal = Modal::new(ctx, "protocol_modal");
+    modal.show(|ui| {
+        modal.title(ui, title);
 
-    egui::Window::new(title)
-        .open(&mut open)
-        .collapsible(false)
-        .min_width(550.)
-        .resizable(false)
-        .show(ctx, |ui| {
-            ui.add_space(8.0);
+        modal.frame(ui, |ui: &mut egui::Ui| {
+            ui.set_width(550.);
 
-            create_button_row(
-                ui,
-                window,
-                TransferType::ZModem,
-                download,
-                "Zmodem",
-                &fl!(crate::LANGUAGE_LOADER, "protocol-zmodem-description"),
-            );
-            create_button_row(
-                ui,
-                window,
-                TransferType::ZedZap,
-                download,
-                "ZedZap",
-                &fl!(crate::LANGUAGE_LOADER, "protocol-zmodem8k-description"),
-            );
-            create_button_row(
-                ui,
-                window,
-                TransferType::XModem,
-                download,
-                "Xmodem",
-                &fl!(crate::LANGUAGE_LOADER, "protocol-xmodem-description"),
-            );
-            create_button_row(
-                ui,
-                window,
-                TransferType::XModem1k,
-                download,
-                "Xmodem 1k",
-                &fl!(crate::LANGUAGE_LOADER, "protocol-xmodem1k-description"),
-            );
-            create_button_row(
-                ui,
-                window,
-                TransferType::XModem1kG,
-                download,
-                "Xmodem 1k-G",
-                &fl!(crate::LANGUAGE_LOADER, "protocol-xmodem1kG-description"),
-            );
-            create_button_row(
-                ui,
-                window,
-                TransferType::YModem,
-                download,
-                "Ymodem",
-                &fl!(crate::LANGUAGE_LOADER, "protocol-ymodem-description"),
-            );
-            create_button_row(
-                ui,
-                window,
-                TransferType::YModemG,
-                download,
-                "Ymodem-G",
-                &fl!(crate::LANGUAGE_LOADER, "protocol-ymodemg-description"),
-            );
-            ui.add_space(8.0);
-            ui.separator();
-            ui.vertical_centered(|ui| {
-                if ui
-                    .button(&fl!(crate::LANGUAGE_LOADER, "phonebook-cancel-button"))
-                    .clicked()
-                {
-                    close = true;
-                }
-            });
+            egui::Grid::new("some_unique_id")
+                .num_columns(2)
+                .spacing([4.0, 8.0])
+                .min_col_width(130.)
+                .min_row_height(24.)
+                .show(ui, |ui| {
+                    for (protocol, title, descr) in &*PROTOCOL_TABLE {
+                        ui.with_layout(ui.layout().with_cross_justify(true), |ui| {
+                            if ui
+                                .selectable_label(false, RichText::new(title).strong())
+                                .clicked()
+                            {
+                                window.initiate_file_transfer(*protocol, download);
+                            }
+                        });
+                        ui.label(RichText::new(descr));
+                        ui.end_row();
+                    }
+                });
         });
-
-    if !open || close {
-        window.mode = MainWindowMode::ShowTerminal;
-    }
+        modal.buttons(ui, |ui| {
+            if modal
+                .button(ui, fl!(crate::LANGUAGE_LOADER, "phonebook-cancel-button"))
+                .clicked()
+            {
+                window.mode = MainWindowMode::ShowTerminal;
+            }
+        });
+    });
+    modal.open();
 }
